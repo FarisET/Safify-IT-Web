2703088aecbcf0f6fd78bcbeeba15b9d8b93c958
FarisET1 <faris@emergtechsolutions.com>
2025-01-11 23:45:11 +0500

    user logout+navbar complete

--- src/pages/User/UserPortal.js
+++ src/pages/User/UserPortal.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 const UserPortal = () => {
     return (
       <div className="container mx-auto px-4 py-8">
-      <h2 className="text-2xl font-semibold mb-4">My Reports</h2>
+      <h2 className="text-2xl font-semibold mb-4">My Tickets</h2>
       </div>
     );
   }
--- src/user components/UserTopNav.js
+++ src/user components/UserTopNav.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { ReactComponent as SafifyIcon } from '../assets/images/safify_it_icon.svg';
-import { FaBars, FaTimes, FaBell, FaQuestionCircle, FaCog, FaBullhorn } from 'react-icons/fa';
+import { FaBars, FaTimes, FaBell, FaQuestionCircle, FaCog, FaBullhorn, FaPowerOff } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
+import useLogout from '../services/logout';
 
 
 
@@ -13,12 +14,30 @@ const UserTopNav = ({ children }) => {
     setIsMenuOpen((prev) => !prev);
   };
 
+  const [showLogoutConfirm, setShowLogoutConfirm] = useState(false);
+  const [logoutLoading, setLogoutLoading] = useState(false);
+
+  // Use the custom hook inside the component
+  const logout = useLogout();
+
+  const handleLogout = async () => {
+    setLogoutLoading(true);
+    await logout();
+    setLogoutLoading(false);
+  };
+
+  const onCloseLogoutModal = async () => {
+    setLogoutLoading(false);
+    setShowLogoutConfirm(false)
+  };
+
+
   return (
     <>
       <div className="fixed top-0 left-0 w-full bg-white border-b border-gray-200 h-16 flex items-center px-4 md:px-6 z-50">
         {/* Logo Section */}
         <div
-          className="flex items-center gap-3 w-[38.2%] flex-grow cursor-pointer"
+          className="flex items-center gap-3 w-auto flex-grow cursor-pointer"
           onClick={() => navigate('/user-portal')}
         >
           <SafifyIcon className="w-8 h-8" />
@@ -55,16 +74,46 @@ const UserTopNav = ({ children }) => {
         </div>
 
         <div className="hidden md:flex items-center gap-4 w-[38.2%] justify-end">
+
           <span
-            className="relative cursor-pointer hover:bg-gray-200 p-2 rounded-full group"
-            onClick={() => navigate('/help')}
+            onClick={() => setShowLogoutConfirm(true)}
+            className="relative cursor-pointer hover:text-red-500 rounded-full group"
           >
-            <FaQuestionCircle />
+            <FaPowerOff />
             <span className="absolute top-full right-0 mt-2 bg-black text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition">
-              Help
-            </span>
+              Logout</span>
           </span>
         </div>
+
+        {showLogoutConfirm && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+            <div className="bg-white p-4 rounded shadow-lg">
+              <h3 className="text-lg font-semibold mb-2">Confirm Logout</h3>
+
+              <p>Are you sure you want to logout?</p>
+
+              {logoutLoading && <p className="mb-4 p-3 rounded text-sky-600 bg-sky-100">Loading...</p>}
+
+
+              <div className="flex justify-end gap-2 mt-4">
+                <button
+                  onClick={() => onCloseLogoutModal()}
+                  className="px-3 py-1 bg-gray-100 text-sm text-gray-700 font-semibold rounded hover:bg-sky-200 transition"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={handleLogout}
+                  className="px-3 py-1 bg-gray-100 text-gray-700 font-semibold rounded hover:text-red-500 transition"
+                >
+                  Logout
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+
+
 
         {/* Hamburger Menu for Small Screens */}
         {isMenuOpen && (
@@ -101,11 +150,11 @@ const UserTopNav = ({ children }) => {
 
             <div className="mt-auto flex flex-col gap-4">
               <button
-                onClick={() => navigate('/help')}
-                className="flex items-center gap-2 hover:bg-gray-200 p-2 rounded mb-4"
+            onClick={() => setShowLogoutConfirm(true)}
+            className="flex font-semibold items-center gap-2 hover:bg-red-200 p-2 rounded mb-4"
               >
-                <FaQuestionCircle />
-                <span>Help</span>
+                <FaPowerOff className='text-red-500' />
+                <span>Logout</span>
               </button>
             </div>
           </div>
